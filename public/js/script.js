e27a2a3914dee272419714b9347c8b7d43217218
Luke Johnson <benjaminlukejohnson@gmail.com>
2023-09-02 15:26:13 -0500

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,46 @@
+
+
+function createThumbnail(shootId, thumbnailPath, creditsText) {
+  const gallery = document.getElementById('gallery');
+  const thumbnail = document.createElement('div');
+  thumbnail.classList.add('thumbnail');
+  thumbnail.innerHTML = `
+    <a href="/shoot/${shootId}">
+      <img src="${thumbnailPath}" alt="Thumbnail">
+    </a>
+    <div class="thumbnail-overlay">${creditsText}</div>
+  `;
+  gallery.appendChild(thumbnail);
+}
+
+// Fetch and display shoot thumbnails on the main page
+async function loadThumbnails() {
+  try {
+    const response = await fetch('/shoots');
+    const shootDataList = await response.json();
+
+    shootDataList.forEach(shootData => {
+      createThumbnail(shootData.id, shootData.thumbnailPath, shootData.firstLineText);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Call the function to load thumbnails when the page loads
+window.addEventListener('DOMContentLoaded', () => {
+  loadThumbnails();
+
+  // Initialize Masonry layout after thumbnails are loaded
+  initMasonry();
+});
+
+// Function to initialize Masonry
+function initMasonry() {
+  const gallery = document.getElementById('gallery');
+  new Masonry(gallery, {
+    itemSelector: '.thumbnail',
+    gutter: 10, // Adjust the gutter between items
+    percentPosition: true, // Use percentage-based sizing for items
+  });
+}
---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+const imagesDir = path.join(__dirname, 'projectImages');
+
+app.use('/projectImages', express.static(imagesDir));
+app.use(express.static(path.join(__dirname, 'public'), { extensions: ['html', 'js'] }));
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/shoot/:shootId', async (req, res) => {
+  const shootId = req.params.shootId;
+  const shootDir = path.join(imagesDir, shootId);
+
+  try {
+    const creditsPath = path.join(shootDir, 'credits.txt');
+    const creditsText = await fs.readFile(creditsPath, 'utf-8');
+
+    // Collect image/video paths in the shoot directory
+    const mediaFiles = await fs.readdir(shootDir);
+    const mediaPaths = mediaFiles.filter(file => !file.includes('credits.txt'));
+
+    res.render('shoot', { shootId, mediaPaths, creditsText });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error loading shoot gallery');
+  }
+});
+
+app.get('/shoots', async (req, res) => {
+  try {
+    const shootFolders = await fs.readdir(imagesDir);
+    const shootDataList = [];
+
+    for (const shootFolder of shootFolders) {
+      const shootDir = path.join(imagesDir, shootFolder);
+
+      try {
+        const creditsPath = path.join(shootDir, 'credits.txt');
+        const creditsText = await fs.readFile(creditsPath, 'utf-8');
+
+        const mediaFiles = await fs.readdir(shootDir);
+        const mediaPaths = mediaFiles.filter(file => !file.includes('credits.txt'));
+
+        shootDataList.push({
+          id: shootFolder,
+          thumbnailPath: path.join('/projectImages', shootFolder, mediaPaths[0]),
+          firstLineText: creditsText.split('\n')[0], // Extract the first line from credits.txt
+        });
+      } catch (error) {
+        console.error(`Error reading data for ${shootFolder}: ${error}`);
+      }
+    }
+
+    res.json(shootDataList);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching shoot data');
+  }
+});
+
