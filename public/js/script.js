f8c7ab0ae28782b4c0616de39af586a24597587a
Luke Johnson <benjaminlukejohnson@gmail.com>
2023-09-06 10:42:59 -0500

    no images updated movie to 1080 + about & contact

--- public/js/script.js
+++ public/js/script.js
@@ -1,59 +1,77 @@
-
-
-function createThumbnail(shootId, thumbnailPath, creditsText) {
-  const gallery = document.getElementById('gallery');
-  const thumbnail = document.createElement('div');
-  thumbnail.classList.add('thumbnail');
-  thumbnail.innerHTML = `
-    <a href="/shoot/${shootId}">
-      <img src="${thumbnailPath}" alt="Thumbnail">
-    </a>
-    <div class="thumbnail-overlay">${creditsText}</div>
-  `;
-  gallery.appendChild(thumbnail);
-}
-
 $(document).ready(function () {
+  // Toggle the menu when the hamburger is clicked
   $(".hamburger").click(function () {
+    if ($(".aboutPC").hasClass("active")) {
+      $(".aboutPC").removeClass("active");
+    } else if ($(".contactPC").hasClass("active")) {
+      $(".contactPC").removeClass("active");
+    }
+    // Toggle the menu
     $(".menu").toggleClass("active");
   });
-});
 
-$(document).ready(function () {
+  // Show aboutPC section when the "ABOUT" link is clicked
   $("#about").click(function () {
-    $(".aboutPC").toggleClass("active");
+    $(".menu").removeClass("active");
+    $(".aboutPC").addClass("active");
+  });
+
+  $("#contact").click(function () {
+    $(".menu").removeClass("active");
+    $(".contactPC").addClass("active");
   });
 });
+
+
+
+
+
+
+
+// function createThumbnail(shootId, thumbnailPath, creditsText) {
+//   const gallery = document.getElementById('gallery');
+//   const thumbnail = document.createElement('div');
+//   thumbnail.classList.add('thumbnail');
+//   thumbnail.innerHTML = `
+//     <a href="/shoot/${shootId}">
+//       <img src="${thumbnailPath}" alt="Thumbnail">
+//     </a>
+//     <div class="thumbnail-overlay">${creditsText}</div>
+//   `;
+//   gallery.appendChild(thumbnail);
+// }
+
+
 
 
 // Fetch and display shoot thumbnails on the main page
-async function loadThumbnails() {
-  try {
-    const response = await fetch('/shoots');
-    const shootDataList = await response.json();
+// async function loadThumbnails() {
+//   try {
+//     const response = await fetch('/shoots');
+//     const shootDataList = await response.json();
 
-    shootDataList.forEach(shootData => {
-      createThumbnail(shootData.id, shootData.thumbnailPath, shootData.firstLineText);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}
+//     shootDataList.forEach(shootData => {
+//       createThumbnail(shootData.id, shootData.thumbnailPath, shootData.firstLineText);
+//     });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
 // Call the function to load thumbnails when the page loads
-window.addEventListener('DOMContentLoaded', () => {
-  loadThumbnails();
+// window.addEventListener('DOMContentLoaded', () => {
+//   loadThumbnails();
 
-  // Initialize Masonry layout after thumbnails are loaded
-  initMasonry();
-});
+//   // Initialize Masonry layout after thumbnails are loaded
+//   initMasonry();
+// });
 
-// Function to initialize Masonry
-function initMasonry() {
-  const gallery = document.getElementById('gallery');
-  new Masonry(gallery, {
-    itemSelector: '.thumbnail',
-    gutter: 10, // Adjust the gutter between items
-    percentPosition: true, // Use percentage-based sizing for items
-  });
-}
+// // Function to initialize Masonry
+// function initMasonry() {
+//   const gallery = document.getElementById('gallery');
+//   new Masonry(gallery, {
+//     itemSelector: '.thumbnail',
+//     gutter: 10, // Adjust the gutter between items
+//     percentPosition: true, // Use percentage-based sizing for items
+//   });
+// }
