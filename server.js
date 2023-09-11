86539924b1639601db49d85ac5cd5cafbc0e3e05
Luke Johnson <benjaminlukejohnson@gmail.com>
2023-09-11 09:32:16 -0500

    adding to heroku

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 const imagesDir = path.join(__dirname, 'projectImages');
 
 app.use('/projectImages', express.static(imagesDir));
 app.use(express.static(path.join(__dirname, 'public'), { extensions: ['html', 'js'] }));
 
 
-app.listen(PORT, () => {
+app.listen(PORT || 3000, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
